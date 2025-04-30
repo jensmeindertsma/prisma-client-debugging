105b8ececbef20aadab090bdc3a8f336aaae37b8
Jens Meindertsma <mail@jensmeindertsma.com>
2025-04-30 23:36:08 +0200

    Temporary fix

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
-import { PrismaClient} from "./generated/prisma/client.js"
+import { PrismaBetterSQLite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaClient } from "./generated/prisma/index.js";
 
-const database = new PrismaClient();
+const adapter = new PrismaBetterSQLite3({
+  url: process.env.ADAPTER_DATABASE_URL as string,
+});
+
+const database = new PrismaClient({ adapter });
 
 const userCount = await database.user.count();
 
-console.log(`Finished counting users: ${userCount} users present`)
+console.log(`Finished counting users: ${userCount} users present`);
 
 await database.user.create({
-    data: {
-        name: "John",
-        email: `john-${new Date().toISOString()}@foo.baz`
-    }
-})
+  data: {
+    name: "John",
+    email: `john-${new Date().toISOString()}@foo.baz`,
+  },
+});
